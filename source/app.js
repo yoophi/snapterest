2a3c7bad84a584febb2736ae8aaec067faa16025
Pyunghyuk Yoo <yoophi@gmail.com>
2016-07-23 12:01:03 +0900

    react 라이브러리 추가. react dom render 샘플 추가.

--- source/app.js
+++ source/app.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var reactElement = React.createElement('h1', {className: 'header'}, 'This is React');
+ReactDOM.render(reactElement, document.getElementById('react-application'));
